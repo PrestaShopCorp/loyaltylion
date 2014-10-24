b7427181f069c5a238556fb376093805580caac5
Dave Clark <me@clarkdave.net>
2014-10-24 09:06:08 +0100

    remove ps_versions_compliancy

--- loyaltylion.php
+++ loyaltylion.php
@@ -47,7 +47,6 @@ class LoyaltyLion extends Module
 		$this->version = '1.0';
 		$this->author = 'LoyaltyLion';
 		$this->need_instance = 0;
-		$this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.6');
 
 		parent::__construct();
 
