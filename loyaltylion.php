33fb645ddf3280f0ad9c58dc57e06293d4ea8227
Dave Clark <me@clarkdave.net>
2014-12-11 17:02:59 +0000

    increment version

--- loyaltylion.php
+++ loyaltylion.php
@@ -44,7 +44,7 @@ class LoyaltyLion extends Module
 	{
 		$this->name = 'loyaltylion';
 		$this->tab = 'advertising_marketing';
-		$this->version = '1.1';
+		$this->version = '1.2';
 		$this->author = 'LoyaltyLion';
 		$this->need_instance = 0;
 
