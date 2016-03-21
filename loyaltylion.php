85d9b68e55fc73de7414fa91bc78faf4d6595802
Patrick Molgaard <patrick@loyaltylion.com>
2016-03-21 15:21:07 +0000

    Use $ namespace for core events

--- loyaltylion.php
+++ loyaltylion.php
@@ -464,7 +464,7 @@ class LoyaltyLion extends Module
 
 		$this->loadLoyaltyLionClient();
 
-		$response = $this->client->activities->track('signup', $data);
+		$response = $this->client->activities->track('$signup', $data);
 
 		if (!$response->success)
 		{
@@ -496,7 +496,7 @@ class LoyaltyLion extends Module
 
 		$this->loadLoyaltyLionClient();
 
-		$response = $this->client->activities->track('review', $data);
+		$response = $this->client->activities->track('$review', $data);
 
 		if (!$response->success)
 		{
@@ -509,7 +509,7 @@ class LoyaltyLion extends Module
 			// reviews do not require moderation, which means this one will be shown immediately and we should
 			// send an update now to approve it right now
 
-			$response = $this->client->activities->update('review', $comment->id, array('state' => 'approved'));
+			$response = $this->client->activities->update('$review', $comment->id, array('state' => 'approved'));
 
 			if (!$response->success)
 			{
@@ -536,7 +536,7 @@ class LoyaltyLion extends Module
 		if (!$object) return;
 
 		$this->loadLoyaltyLionClient();
-		$response = $this->client->activities->update('review', $object->id, array('state' => 'declined'));
+		$response = $this->client->activities->update('$review', $object->id, array('state' => 'declined'));
 
 		if (!$response->success)
 		{
@@ -562,7 +562,7 @@ class LoyaltyLion extends Module
 		if (!$object) return;
 
 		$this->loadLoyaltyLionClient();
-		$response = $this->client->activities->update('review', $object->id, array('state' => 'approved'));
+		$response = $this->client->activities->update('$review', $object->id, array('state' => 'approved'));
 
 		if (!$response->success)
 		{
